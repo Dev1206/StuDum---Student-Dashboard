d5299234b92256bb24a6e82e4920fa8e61e3bb5c
Dev1206 <106542756+Dev1206@users.noreply.github.com>
2024-12-10 02:33:47 -0500

    Update chatService.jsx

--- src/services/chatService.jsx
+++ src/services/chatService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001/api';
+const API_URL = 'https://studum-student-dashboard.onrender.com/api';
 
 export const chatService = {
   // Get all chats for a user
